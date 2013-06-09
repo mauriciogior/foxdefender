a3d47e229f546035783381791fec69a189ee46d3
Marcio Paulo Mello Martins <marciopmm@gmail.com>
2013-06-09 02:24:18 -0300

    Teste do som

---
+++ tests/main.js
@@ -0,0 +1,29 @@
+enchant();
+
+window.onload = function(){
+    var game = new Core(320, 320); // game stage
+    game.preload('chara1.png'); // preload image
+	game.preload('sounds/shot01.mp3');
+    game.fps = 20;
+
+    game.onload = function(){
+        var bear = new Sprite(32, 32);
+        bear.image = game.assets['chara1.png'];
+        game.rootScene.addChild(bear);
+        bear.frame = [6, 6, 7, 7];   // select sprite frame
+        
+        bear.tl.moveBy(288, 0, 90)   // move right
+            .scaleTo(-1, 1, 5)      // turn left
+            .moveBy(-288, 0, 90)     // move left
+            .scaleTo(1, 1, 5)       // turn right
+            .loop();                 // loop it
+			
+		bear.addEventListener('touchstart', function() {
+			var musica = new Sound();
+			musica.src = game.assets['sounds/shot01.mp3'];
+			musica.play();
+		});
+    };
+
+    game.start();
+};
